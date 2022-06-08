1f87fdcf432799a73fd6022d3c6dcb73cef1571e
Cody Falzone <cfalz002@ucr.edu>
2022-06-08 09:17:12 -0700

    Initial Commit

---
+++ Tangibles/TangiblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangiblesApp.swift
+//  Tangibles
+//
+//  Created by Cody Falzone on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TangiblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
